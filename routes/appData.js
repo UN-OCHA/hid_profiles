6a36bb0e68ae21b9203de46b7291c6e9423e4d6d
Tim Martin <tmartin@phase2technology.com>
2015-02-10 15:26:30 -0600

    Added protected roles feature

--- routes/appData.js
+++ routes/appData.js
@@ -44,10 +44,49 @@ function getAppRoles(callback) {
   });
 }
 
+function getProtectedRoles(callback) {
+  async.series([
+    function (cb) {
+      Cache.findOne({"name": "protected_roles"}, function (err, doc) {
+        if (err) {
+          return cb(err, null);
+        }
+        else if (doc && doc.data) {
+          var ops = [];
+          _.forEach(doc.data, function (item) {
+            _.forEach(item, function (role) {
+              if (role.label && role.label.length) {
+                var op = {
+                  id: role.id,
+                  name: role.label
+                };
+                ops.push(op);
+              }
+            });
+          });
+          return cb(null, ops);
+        }
+        return cb(null, null);
+      });
+    }
+  ], function (err, results) {
+   _.forEach(results, function (items) {
+     if (items && items.length) {
+       protected_roles = protected_roles.concat(items);
+     }
+   });
+   protected_roles = protected_roles.sort(function(a, b) { return (a.name > b.name) ? 1 : -1; });
+   return callback(null, protected_roles);
+  });
+}
+
 function getAppData(req, res, next) {
   getAppRoles(function (err, data) {
     res.send({roles: data});
-    next();
+  });
+
+  getProtectedRoles(function (err, data) {
+    res.send({protectedRoles : data});
   });
 }
 
