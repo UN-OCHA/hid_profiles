47416d724f664e31daf0dd30a69dc582a9f9d3e0
Tim Martin <tmartin@phase2technology.com>
2015-02-10 18:08:56 -0600

    Broke out protected roles into its own file

--- routes/appData.js
+++ routes/appData.js
@@ -1,47 +1,12 @@
 var roles = require('../lib/roles.js');
-
-function getProtectedRoles(callback) {
-  async.series([
-    function (cb) {
-      Cache.findOne({"name": "protected_roles"}, function (err, doc) {
-        if (err) {
-          return cb(err, null);
-        }
-        else if (doc && doc.data) {
-          var ops = [];
-          _.forEach(doc.data, function (item) {
-            _.forEach(item, function (role) {
-              if (role.label && role.label.length) {
-                var op = {
-                  id: role.id,
-                  name: role.label
-                };
-                ops.push(op);
-              }
-            });
-          });
-          return cb(null, ops);
-        }
-        return cb(null, null);
-      });
-    }
-  ], function (err, results) {
-   _.forEach(results, function (items) {
-     if (items && items.length) {
-       protected_roles = protected_roles.concat(items);
-     }
-   });
-   protected_roles = protected_roles.sort(function(a, b) { return (a.name > b.name) ? 1 : -1; });
-   return callback(null, protected_roles);
-  });
-}
+var protectedRoles = require('../lib/protectedRoles.js');
 
 function getAppData(req, res, next) {
   roles.get(function (err, data) {
     res.send({roles: data});
   });
 
-  getProtectedRoles(function (err, data) {
+  protectedRoles.get(function (err, data) {
     res.send({protectedRoles : data});
   });
 }
