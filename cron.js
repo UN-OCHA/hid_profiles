d83109de05ef5a86fa28981d9c455490b0be1681
Joe Turgeon <jturgeon@phase2technology.com>
2015-03-30 15:04:33 -0500

    Updating operations cron procedure to use new HR.info API process.

--- cron.js
+++ cron.js
@@ -1,38 +1,12 @@
 var models = require('./models'),
   config = require('./config'),
+  operations = require('./lib/operations'),
   disasters = require('./lib/disasters'),
   async = require('async'),
   restify = require('restify');
 
 async.series([
-  function (cb) {
-    // Fetch the active operations list from HR.info
-    var client = restify.createJsonClient({
-      url: config.hrinfoBaseUrl,
-      version: '*'
-    });
-    client.get("/hid/operations", function(err, req, res, obj) {
-      if (res.statusCode == 200 && res.body) {
-        var obj = JSON.parse(res.body);
-        if (!obj) {
-          return cb();
-        }
-        models.Cache.update({"name": "operations"}, {"name": "operations", "data": obj}, {"upsert": true}, function (err, doc) {
-          if (err) {
-            console.log("ERROR: Error when updating document.", err);
-          }
-          else {
-            console.log("SUCCESS: Retrieved and stored operation data.");
-          }
-          return cb();
-        });
-      }
-      else {
-        console.log("ERROR: Fetched /hid/operations. Did not receive successful response.");
-        return cb();
-      }
-    });
-  },
+  operations.buildCache,
   function (cb) {
     // Fetch the functional roles list from HR.info
     var client = restify.createJsonClient({
