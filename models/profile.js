32aec96ba0d23646cf6ca2046ad79264ca856e22
Eduardo Velez <edvelez1@gmail.com>
2015-05-06 17:21:11 -0400

    provide route for saving personal contact list

--- models/profile.js
+++ models/profile.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var contactListSchema = new Schema({
+  name:     String,
+  contacts: [ String ],
+});
+
 var orgEditorRoleSchema = new Schema({
   locationId:         String,
   organizationId:     String,
@@ -8,19 +13,20 @@ var orgEditorRoleSchema = new Schema({
 });
 
 var profileSchema = new mongoose.Schema({
-  userid:         String,
-  nameFamily:     String,
-  nameGiven:      String,
-  email:          String,
-  ochaContent:    { topics: [ String ] },
-  created:        Number, // timestamp
-  revised:        Number, // timestamp
-  firstUpdate:    Number, // timestamp
-  status:         Boolean,
-  _contacts:      [{ type: Schema.Types.ObjectId, ref: 'Contact' }],
-  roles:          [ String ],
-  orgEditorRoles: [ orgEditorRoleSchema ],
-  verified:       Boolean
+  userid:             String,
+  nameFamily:         String,
+  nameGiven:          String,
+  email:              String,
+  ochaContent:        { topics: [ String ] },
+  created:            Number, // timestamp
+  revised:            Number, // timestamp
+  firstUpdate:        Number, // timestamp
+  status:             Boolean,
+  _contacts:          [{ type: Schema.Types.ObjectId, ref: 'Contact' }],
+  roles:              [ String ],
+  orgEditorRoles:     [ orgEditorRoleSchema ],
+  verified:           Boolean,
+  contactLists:  [ contactListSchema ],
 });
 
 mongoose.model('Profile', profileSchema);
--- server.js
+++ server.js
@@ -38,6 +38,7 @@ server.get(versionPrefix + 'app/data', middleware.require.appOrUser, routes.appD
 server.get(versionPrefix + 'profile/view', middleware.require.appOrUser, routes.profileView.get);
 server.post(versionPrefix + 'profile/view', middleware.require.appOrUser, routes.profileView.get);
 server.post(versionPrefix + 'profile/delete', middleware.require.appOrUser, routes.profileDelete.postAccess, routes.profileDelete.post);
+server.post(versionPrefix + 'profile/save', middleware.require.appOrUser, routes.profileSave.postAccess, routes.profileSave.post);
 // Disabled until this is refactored/combined with contactSave.post.
 //server.post(versionPrefix + 'profile/save/:uid', middleware.require.appOrUser, routes.contactSave.postAccess, routes.profileSave.post);
 
