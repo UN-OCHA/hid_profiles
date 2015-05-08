5f9e5381388f7655917fffc41237e5935732ef9d
Joe Turgeon <jturgeon@phase2technology.com>
2015-05-08 11:22:57 -0500

    UNCID-442: Adding data ingestion of Organization Types from the HR.info API to the cron script and app data.

--- cron.js
+++ cron.js
@@ -2,6 +2,7 @@ var operations = require('./lib/operations'),
   bundles = require('./lib/bundles'),
   offices = require('./lib/offices'),
   protectedRoles = require('./lib/protectedRoles'),
+  orgTypes = require('./lib/orgTypes'),
   disasters = require('./lib/disasters'),
   async = require('async');
 
@@ -10,8 +11,9 @@ async.auto({
   bundles: bundles.fetchBundles,
   offices: offices.fetchOffices,
   protectedRoles: protectedRoles.buildCache,
+  orgTypes: orgTypes.buildCache,
   disasters: disasters.buildCache,
-  appData: ['operations', 'bundles', 'offices', 'protectedRoles', 'disasters', operations.buildAppData]
+  appData: ['operations', 'bundles', 'offices', 'protectedRoles', 'orgTypes', 'disasters', operations.buildAppData]
 },
 function (err, results) {
   if (err) {
