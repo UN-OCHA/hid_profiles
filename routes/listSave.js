ac1e93427ee6d730cb717216bde98dc947188e1e
Jonathan Adams <adams.jonathan@outlook.com>
2015-07-24 10:42:11 -0400

    UNCID-699: Refactored update logic to use async.

--- routes/listSave.js
+++ routes/listSave.js
@@ -1,25 +1,41 @@
-var List = require('../models').List;
+var async = require('async'),
+  List = require('../models').List;
 
 function post(req, res, next) {
   // TODO: Add support for adding contacts in a later ticket.
   // TODO: Add access permissions in a later ticket.
   // If an id exists then update the contact list.
   if (req.body._id) {
-    List.findById(req.body._id, function(err, list){
+    var updatedList = {};
+
+    async.series([
+      function(cb) {
+        List.findById(req.body._id, function(err, list){
+          if (err) {
+            return cb(err);
+          }
+
+          updatedList = list;
+          cb();
+        });
+      },
+      function(cb) {
+        updatedList.name = req.body.name;
+        updatedList.users = req.body.users;
+        updatedList.contacts = req.body.contacts;
+
+        updatedList.save(function(err){
+          if (err) {
+            return cb(err);
+          }
+          cb();
+        });
+      }
+    ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
       if (err) {
         return res.json({status: "error", message: "Could not update contact list."});
       }
-
-      list.name = req.body.name;
-      list.users = req.body.users;
-      list.contacts = req.body.contacts;
-
-      list.save(function(err){
-        if (err) {
-          return res.json({status: "error", message: "Could not update contact list."});
-        }
-        res.json({ status: "ok", message: "Contact list updated" });
-      });
+      return res.json({status: "ok", message: "Contact list updated."});
     });
   } else {
     list = new List();
