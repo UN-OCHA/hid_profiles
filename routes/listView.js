330c12112ad05e740c9e512fec63070e700e7674
Jonathan Adams <adams.jonathan@outlook.com>
2015-08-26 11:58:05 -0400

    UNCID-707: Locked crisis restrictions.

--- routes/listView.js
+++ routes/listView.js
@@ -1,7 +1,24 @@
 var   _ = require('lodash'),
-  List = require('../models').List;
+  operations = require('../lib/operations'),
+  List = require('../models').List,
+  Profile = require('../models').Profile;
 
 function get(req, res, next) {
+  var lockedOperations = [];
+  operations.getLockedOperations(function (err, _lockedOperations) {
+    lockedOperations = _lockedOperations;
+  });
+
+  if (req.apiAuth.mode === 'user' && req.apiAuth.userId) {
+    Profile.findOne({userid: req.apiAuth.userId}, function (err, profile) {
+      if (!err && profile && profile._id) {
+        if (!profile.verified) {
+          req.limitContacts = true;
+        }
+      }
+    });
+  }
+
   // Only find lists that users have access to.
   if (req.query.id) {
     List.findOne({_id:req.query.id})
@@ -10,6 +27,18 @@ function get(req, res, next) {
       if (err) {
         return res.json({status: "error", message: "There was an error retrieving the custom contact list."});
       }
+
+      // Trusted Apis & Verified User
+      if (req.limitContacts) {
+        var contacts = [];
+        _.forEach(list.contacts, function(contact) {
+          if (lockedOperations.indexOf(contact.locationId) == -1) {
+            contacts.push(contact);
+          }
+        });
+        list.contacts = contacts;
+      }
+
       res.json({ status: "ok", lists: list });
     });
   } else {
