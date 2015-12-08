43a75e6c4495c544990f5a1a312c66efee4ab01f
Guillaume Viguier <guillaume@viguierjust.com>
2015-12-08 09:34:04 +0100

    UNCID-893: added references to locations

--- models/service.js
+++ models/service.js
@@ -14,7 +14,8 @@ var serviceSchema = new Schema({
   mc_api_key: {type: String},
   mc_list: { id: String, name: String},
   status: { type: Boolean, default: true},
-  hidden: { type: Boolean, default: false}
+  hidden: { type: Boolean, default: false},
+  locations: [ { name: String, remote_id: String } ]
 });
 
 serviceSchema.pre('remove', function (next) {
