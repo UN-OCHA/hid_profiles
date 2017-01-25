ccb4d73b279758ba50a39ab1a9cabcc00e7dc0a8
Guillaume Viguier <guillaume@viguierjust.com>
2017-01-25 10:56:41 +0100

    Fixed issue with list view

--- routes/listView.js
+++ routes/listView.js
@@ -852,8 +852,10 @@ function getAll(req, res, next) {
                 }
               }
               else {
-                count--;
-                log.warn({'type': 'listView:error', 'message': 'An error occured while requesting the list owner: ' + err.message, 'error': err});
+                //count--;
+                if (err) {
+                  log.warn({'type': 'listView:error', 'message': 'An error occured while requesting the list owner: ' + err.message, 'error': err});
+                }
                 cb();
               }
             });
